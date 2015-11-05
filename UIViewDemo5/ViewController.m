2ed0458d14566ee4fbc8cf454f896ebda54b3245
MCJ <MCJ@MCJ.local>
2015-11-05 14:47:57 +0800

    Initial Commit

---
+++ UIViewDemo5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIViewDemo5
+//
+//  Created by MCJ on 15/11/5.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
